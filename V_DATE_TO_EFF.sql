3416967b97780efa41e772faf21b8992c4cfd5d4
Michael A. Kelly Ph.D <mkelly0826@gmail.com>
2015-09-11 08:54:09 -0400

    Changes to be committed: 	new file:   TABLE_DDL_FILES/TABLE_DDL_FILES.zip 	new file:   U_BACKUP_VIEWS_1.sql 	new file:   U_REFRESH_MVS.sql 	new file:   V_DATE_TO_EFF.sql
     Changes not staged for commit:
    	deleted:    Generated-20150904074733.sql
    	deleted:    Generated-20150904074826.sql
    	modified:   U_10_SKU_BASE.sql

---
+++ V_DATE_TO_EFF.sql
@@ -0,0 +1,20 @@
+--------------------------------------------------------
+--  DDL for Function V_DATE_TO_EFF
+--------------------------------------------------------
+
+  CREATE OR REPLACE FUNCTION "SCPOMGR"."V_DATE_TO_EFF" ( IN_DATE DATE ) 
+       RETURN NUMBER IS
+EFF NUMBER;
+BEGIN
+   EFF := (trunc(IN_DATE) - to_date('19700101','YYYYMMDD'))*1440;
+   RETURN EFF;
+   EXCEPTION
+     WHEN NO_DATA_FOUND THEN
+       NULL;
+     WHEN OTHERS THEN
+       -- Consider logging the error and then re-raise
+       RAISE;
+END V_DATE_TO_EFF;
+
+/
+
